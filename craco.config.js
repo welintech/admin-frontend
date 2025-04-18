9d1492576a84ba3fa655ec881c9bde11a2d4fa8e
Jivan Toshniwal <jtoshniwal@adobe.com>
2025-04-19 00:44:45 +0530

    Fix

---
+++ craco.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  devServer: {
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      // Add your custom middleware here if needed
+      return middlewares;
+    },
+  },
+  webpack: {
+    configure: {
+      optimization: {
+        splitChunks: {
+          chunks: 'all',
+          minSize: 20000,
+          maxSize: 244000,
+          cacheGroups: {
+            vendor: {
+              test: /[\\/]node_modules[\\/]/,
+              name: 'vendors',
+              chunks: 'all',
+            },
+          },
+        },
+      },
+    },
+  },
+};
