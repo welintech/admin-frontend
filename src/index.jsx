a4b83eaa31a2cb223fdcda8a6d28ae3b1efa4988
Jivan Toshniwal <jtoshniwal@adobe.com>
2025-05-25 16:00:54 +0530

    fix deployment

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './hooks/useAuth';
