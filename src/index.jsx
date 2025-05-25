4843e504e839fd9d11f3019eaac0c339137035ed
Jivan Toshniwal <jtoshniwal@adobe.com>
2025-05-25 20:26:29 +0530

    Fix some warnings

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <AuthProvider>
           <SocketProvider>
             <App />
